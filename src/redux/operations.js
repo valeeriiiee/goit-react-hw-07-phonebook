aee6e8228825d6b39615613f55aa64cbd3a122c9
valeeriiiee <obleavalerie@gmail.com>
2024-07-23 23:08:44 +0800

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -8,14 +8,38 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://669e623c9a1bda368006745e.mockapi.io/api';
 
-export const fetchContacts = () => async dispatch => {
-  try {
-    dispatch(fetchingContactsInProgress());
-
-    const response = await axios.get('/contacts');
-
-    dispatch(fetchingContactsSuccess(response.data));
-  } catch (e) {
-    dispatch(fetchingContactsError(e.message));
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-};
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contactData, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', contactData);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      await axios.delete('/contacts', contactId);
+      return contactId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
