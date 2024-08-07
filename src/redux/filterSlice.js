7b567b025d985f9112e044c4c204854cea8d6636
valeeriiiee <obleavalerie@gmail.com>
2024-08-07 23:19:45 +0800

    Update filterSlice.js

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,18 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { setFilter } from './actions';
-
-const initialFilterState = '';
 
 export const filterSlice = createSlice({
   name: 'filter',
-  initialState: initialFilterState,
+  initialState: '',
   reducers: {
-    setFilter: {
-      reducer(_state, action) {
-        return action.payload;
-      },
+    setFilter(state, action) {
+      return action.payload;
     },
   },
 });
 
 export const { setFilter } = filterSlice.actions;
+export default filterSlice.reducer;
