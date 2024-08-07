aa8a19fd3abddb9e70c83b89d21d341f9e30e72f
valeeriiiee <obleavalerie@gmail.com>
2024-08-07 23:20:08 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,31 @@
+// App.jsx
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addContact, deleteContact, setFilter } from '../redux/actions';
-import { getContacts, getFilter } from '../redux/selectors';
 import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-
-const retrieveInitialContacts = () => {
-  const savedContacts = JSON.parse(localStorage.getItem('contacts'));
-  if (savedContacts && savedContacts.length > 0) {
-    return savedContacts;
-  }
-  console.log('Using default contacts');
-  return [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-};
+import { ContactList } from './ContactList/ContactList';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from '../redux/filterSlice';
+import { fetchContacts, addContact, deleteContact } from '../redux/operations';
+import {
+  selectFilter,
+  selectVisibleContacts,
+  selectIsLoading,
+  selectError,
+} from '../redux/selectors';
 
 export const App = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
+  const visibleContacts = useSelector(selectVisibleContacts);
+  const filter = useSelector(selectFilter);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const initialContacts = retrieveInitialContacts();
-    initialContacts.forEach(contact => dispatch(addContact(contact)));
+    dispatch(fetchContacts());
   }, [dispatch]);
 
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
   const handleAddContact = newContact => {
-    const duplicateContact = contacts.find(
-      contact => contact.name === newContact.name
-    );
-
-    if (duplicateContact) {
-      alert(`${newContact.name} is already in your contacts.`);
-      return;
-    }
-
     dispatch(addContact(newContact));
   };
 
@@ -55,7 +37,7 @@ export const App = () => {
     dispatch(setFilter(newFilter));
   };
 
-  const filterContact = () => {
+  const filterContacts = contacts => {
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
@@ -64,16 +46,21 @@ export const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContact={handleAddContact} contacts={contacts} />
+      <ContactForm addContact={handleAddContact} contacts={visibleContacts} />
 
       <h2>Contacts</h2>
       <Filter filter={filter} setFilter={handleSetFilter} />
-      <ContactList
-        filterContact={filterContact}
-        deleteContact={handleDeleteContact}
-      />
+      {isLoading && (
+        <b style={{ display: 'block', padding: '0 0 20px 10px' }}>Loading...</b>
+      )}
+      {error && <b>Error: {error}</b>}
+      {visibleContacts && (
+        <ContactList
+          filterContact={filterContacts} // Function for filtering contacts
+          deleteContact={handleDeleteContact}
+          contacts={visibleContacts}
+        />
+      )}
     </div>
   );
 };
-
-export default App;
