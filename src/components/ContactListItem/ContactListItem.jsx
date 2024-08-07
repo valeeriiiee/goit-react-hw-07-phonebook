25f1cd3ba6f3b489a26d8097e817d51185bd2db3
valeeriiiee <obleavalerie@gmail.com>
2024-08-07 23:20:02 +0800

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -3,19 +3,23 @@ import css from './ContactListItem.module.css';
 import PropTypes from 'prop-types';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export const ContactListItem = ({ filteredContact, deleteContact }) => {
+export const ContactListItem = ({ contact, deleteContact }) => {
+  if (!contact) {
+    return null; // or return a placeholder or error message
+  }
+
   const handleDelete = () => {
-    deleteContact(filteredContact.id);
+    deleteContact(contact.id);
     Notify.success(
-      `${filteredContact.name} was successfully deleted from your contacts!`,
+      `${contact.name} was successfully deleted from your contacts!`,
       { position: 'center-top' }
     );
   };
 
   return (
     <li className={css.contactListItem}>
-      <p>{filteredContact.name}:</p>
-      <p className={css.contactAlign}>{filteredContact.number}</p>
+      <p>{contact.name}:</p>
+      <p className={css.contactAlign}>{contact.phone}</p>
       <button className={css.btnDelete} onClick={handleDelete}>
         Delete
       </button>
@@ -24,6 +28,16 @@ export const ContactListItem = ({ filteredContact, deleteContact }) => {
 };
 
 ContactListItem.propTypes = {
-  filteredContact: PropTypes.object.isRequired,
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }),
   deleteContact: PropTypes.func.isRequired,
 };
+
+ContactListItem.defaultProps = {
+  contact: null, // Ensure a default value if not provided
+};
+
+export default ContactListItem;
